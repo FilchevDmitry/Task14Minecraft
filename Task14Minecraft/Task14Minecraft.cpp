a9693c186edf65a41c2c962cbd303baf0259fbfb
DmityFilchev <filchev_dmitry@mail.ru>
2022-04-26 16:17:42 +0300

    '12'

--- Task14Minecraft/Task14Minecraft.cpp
+++ Task14Minecraft/Task14Minecraft.cpp
@@ -5,23 +5,23 @@ int main()
 {
     cout << "Minecraft!\n";
     int h=0;
-    bool mine[3][3][4];
-    for (int i = 0; i < 4; i++)
+    bool mine[10][5][5];
+    for (int i = 0; i < 10; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < 5; j++)
         {
 
-            for (int k = 0; k < 3; k++)
+            for (int k = 0; k < 5; k++)
             {
                 mine[i][j][k] = true;
             }
         }
     }
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 10; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < 5; j++)
         {
-            for (int k = 0; k < 3; k++)
+            for (int k = 0; k < 5; k++)
             {
                 cout << mine[i][j][k] << " ";
             }
